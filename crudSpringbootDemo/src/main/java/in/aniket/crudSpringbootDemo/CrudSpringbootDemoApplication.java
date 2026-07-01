22b23ade720577fe9092aaf53f6b067faf7f9567
Aniket Bodkhe <aniketbodkhe722@gmail.com>
2026-07-01 15:14:24 +0530

    Add project for crudspringboot application

---
+++ crudSpringbootDemo/src/main/java/in/aniket/crudSpringbootDemo/CrudSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package in.aniket.crudSpringbootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringbootDemoApplication.class, args);
+	}
+
+}
---
+++ crudSpringbootDemo/src/test/java/in/aniket/crudSpringbootDemo/CrudSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.aniket.crudSpringbootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
