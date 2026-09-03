071e8cbc470711929c38d78ffe57d995f058e4cf
Aniket Bodkhe <aniketbodkhe722@gmail.com>
2026-09-03 19:19:57 +0530

    Add the code for DTOs

---
+++ crudSpringbootDemo/src/main/java/in/aniket/crudSpringbootDemo/dto/UpdateResponseDto.java
@@ -0,0 +1,14 @@
+package in.aniket.crudSpringbootDemo.dto;
+
+import java.time.LocalDateTime;
+
+public class UpdateResponseDto {
+    private Long id;
+    private String name;
+    private int age;
+    private String email;
+    private int rollNo;
+    private String subject;
+    private String message;
+    private LocalDateTime updateddAt;
+}
