c63614a4dd703c9ab49bb768d2b1ccec8a2a7565
Aniket Bodkhe <aniketbodkhe722@gmail.com>
2026-09-07 18:46:20 +0530

    Add final code for profileDemo

--- profileDemo/src/main/java/com/example/profileDemo/DummyNotificationServiceImpl.java
+++ profileDemo/src/main/java/com/example/profileDemo/DummyNotificationServiceImpl.java
@@ -1,8 +1,10 @@
 package com.example.profileDemo;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("dev")
 public class DummyNotificationServiceImpl implements NotificationService{
     @Override
     public String send() {
