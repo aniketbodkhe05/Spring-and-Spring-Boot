d18199206cbda18f96bc4dbf5d434db5f35646fa
Aniket Bodkhe <aniketbodkhe722@gmail.com>
2026-09-08 21:56:53 +0530

    Add code for filters in spring boot

---
+++ filtersDemo/src/main/java/com/filtersdemo/filtersDemo/filter/LoggingFilter.java
@@ -0,0 +1,24 @@
+package com.filtersdemo.filtersDemo.filter;
+
+import jakarta.servlet.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class LoggingFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
