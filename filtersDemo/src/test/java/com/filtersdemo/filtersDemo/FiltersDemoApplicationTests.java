d2e980340524d8c0143864c588ed52ef9b335c85
Aniket Bodkhe <aniketbodkhe722@gmail.com>
2026-09-08 21:44:29 +0530

    Add code for filters in spring boot

---
+++ filtersDemo/src/test/java/com/filtersdemo/filtersDemo/FiltersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filtersdemo.filtersDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltersDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
