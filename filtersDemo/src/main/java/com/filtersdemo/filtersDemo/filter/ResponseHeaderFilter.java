65d34d2c9127c3e526255a4daad56c7782c9cb0c
Aniket Bodkhe <aniketbodkhe722@gmail.com>
2026-09-11 23:22:34 +0530

    Add new filter method in spring project

--- filtersDemo/src/main/java/com/filtersdemo/filtersDemo/filter/ResponseHeaderFilter.java
+++ filtersDemo/src/main/java/com/filtersdemo/filtersDemo/filter/ResponseHeaderFilter.java
@@ -1,4 +1,13 @@
 package com.filtersdemo.filtersDemo.filter;
 
-public class ResponseHeaderFilter {
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+public class ResponseHeaderFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest= (HttpServletRequest) request;
+    }
 }
