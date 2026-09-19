eec3edc8fce58fa0d8c9c23d7739e2f04442950c
Aniket Bodkhe <aniketbodkhe722@gmail.com>
2026-09-19 14:50:51 +0530

    Add new Interceptors logic basic program by overriding handler methods

--- filtersDemo/src/main/java/com/filtersdemo/filtersDemo/Interceptors/LoggingInterceptor.java
+++ filtersDemo/src/main/java/com/filtersdemo/filtersDemo/Interceptors/LoggingInterceptor.java
@@ -1,4 +1,20 @@
 package com.filtersdemo.filtersDemo.Interceptors;
 
-public class LoggingInterceptor {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler){
+        System.out.println("PreHandle called");
+        return true;
+
+    }
 }
