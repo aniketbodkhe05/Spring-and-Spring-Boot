b3ec81c79c5e063842fec73488cd194ea9276297
Aniket Bodkhe <aniketbodkhe722@gmail.com>
2026-09-19 15:04:00 +0530

    Add new Interceptors logic

---
+++ filtersDemo/src/main/java/com/filtersdemo/filtersDemo/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.filtersdemo.filtersDemo.config;
+
+import com.filtersdemo.filtersDemo.Interceptors.LoggingInterceptor;
+import jakarta.servlet.http.WebConnection;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public LoggingInterceptor loggingInterceptor;
+
+    public WebConfig(LoggingInterceptor interceptor){
+        this.loggingInterceptor=interceptor;
+    }
+
+    @Override
+    public void addInterceptor(InterceptorRegistry registry){
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
